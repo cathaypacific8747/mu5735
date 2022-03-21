b342fd3d5bb480ebbe959b4e5a352577f60d2ff4
cathaypacific8747 <abrahamcheung2004@gmail.com>
2022-03-21 21:05:40 +0800

    correct data and prepare for colour gradient implementations

--- kml.py
+++ kml.py
@@ -1,6 +1,7 @@
 import pandas
 from fastkml import kml
 from fastkml.geometry import Geometry
+from fastkml.styles import LineStyle
 from shapely.geometry import Point, LineString
 
 k = kml.KML()
@@ -8,13 +9,17 @@ d = kml.Document()
 line = []
 
 df = pandas.read_csv('combined.csv')
-for i in df.itertuples():
-    line.append(Point(i.lng, i.lat, i.altitude * 0.3048))
+df_s = [(i.lng, i.lat, i.altitude * 0.3048) for i in df.itertuples()]
 
-p = kml.Placemark()
-p.geometry = Geometry(geometry=LineString(line), altitude_mode="absolute")
+for j in range(len(df_s)-1):
+    p = kml.Placemark()
+    p.geometry = Geometry(geometry=LineString([
+        Point(*df_s[j]),
+        Point(*df_s[j+1]),
+    ]), altitude_mode="absolute")
 
-d.append(p)
+    d.append(p)
+
 k.append(d)
 
 with open('out.kml', 'w+', encoding='utf-8') as f:
